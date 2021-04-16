99353cd83770dfd9a124a751711a191bd73e9f79
Shuxin Zhang <s.x.zhang1201@gmail.com>
2021-04-16 10:55:21 +0200

    Clean Main Branch

--- config.py
+++
@@ -1,3 +0,0 @@
-
-DATA_DICTIONARY = "Data/Raw data/Data_Dictionary_14_04_2021.xlsx"
-
--- run_preprocessing.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-# Load stored data
-df = pd.read_pickle("Data/my_test_df.pickle")
-
-print(df)
